af5b839989bd4bd17e34d71117c24a7951622502
Huw Taylor <huw_taylor@hotmail.co.uk>
2017-02-09 17:55:47 +0000

    got pie chart working for minimum deaths per country

--- stats-setup.js
+++ stats-setup.js
@@ -23,7 +23,40 @@ $.ajax({
             country_deaths[country].civ += isNaN(civ) ? 0 : civ;
         });
 
-        console.log(country_deaths);
+        var dataset = [];
+
+        Object.keys(country_deaths).forEach(function(key, value) {
+            dataset.push( {label: key, count: country_deaths[key].min } )
+        });
+
+        var color  = d3.scaleOrdinal(d3.schemeCategory20b);
+        var width  = 360;
+        var height = 360;
+        var radius = Math.min(width, height) / 2;
+
+        var svg = d3.select('#country-pie-chart')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
+        var arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius);
+
+        var pie = d3.pie()
+            .value(function(d) { return d.count; })
+            .sort(null);
+
+        var path = svg.selectAll('path')
+            .data(pie(dataset))
+            .enter()
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', function(d) {
+                return color(d.data.label);
+            });
 
     }
 });
